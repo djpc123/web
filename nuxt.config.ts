54706f93339e544b9d3ea5f7f8dda3794ee73ab7
Chris Ewbank <chris@djpc.org.uk>
2026-04-18 05:45:37 +0000

    chore: update dependabot to monthly, add custom favicon and page title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Chris Ewbank | Engineering Leader',
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }
+      ]
+    }
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: [
